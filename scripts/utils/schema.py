3e9f3c8db6592755f6b1523334648e0ee901c290
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2025-11-02 14:46:16 +0800

    Add schema.py to utils directory

---
+++ scripts/utils/schema.py
@@ -0,0 +1,18 @@
+def base_song_template():
+    return {
+        "unique_id": "",
+        "title": "",
+        "artist": "",
+        "album": "",
+        "release_date": "",
+        "studio": "",
+        "writers": [],
+        "producers": [],
+        "language": "",
+        "lyrics_original": "",
+        "lyrics_translation": {},
+        "social_media": {},
+        "instrument_maker": [],
+        "source_url": "",
+        "extra_info": {},
+    }
